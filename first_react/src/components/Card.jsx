10bc879ff1538cd3607dd9d63d38113ad1f97c95
tamim <t.a.fahim24@gmail.com>
2025-11-09 04:58:26 +0600

    second commit

---
+++ first_react/src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+
+function Card() {
+    return (
+        <div className="card">
+            <h1>Hello!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, consequatur ratione. Temporibus corporis unde officiis iste id illum labore perspiciatis deleniti, ipsam minima iure laudantium velit eum assumenda, obcaecati repellat!</p>
+        </div>
+    )
+}
+
+export default Card
